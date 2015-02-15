5a85232c09768e6eba40f9af041a81cfb601bc0f
Annette Altvater <annette.altvater@dpc-tz.com>
2015-02-15 20:05:09 +0300

    Updates to the read me file

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # datasciencecoursera
 My first repo
+I am changing the README file in my local repo and sync it with the remote repo
--- test.R
+++ test.R
@@ -1,1 +1,6 @@
+<<<<<<< Updated upstream
 ## I want to add/commit/pull a edit from a local repository to the remote repository.
+=======
+## I want to add/commit/pull a edit from a local repository to the remote repository.
+# yes that worked out but I don't kow how to merge the 'test' file into the ReadME file
+>>>>>>> Stashed changes
